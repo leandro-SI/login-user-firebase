da6ca4772ddf2097eea7594c6e872eab6517f0c9
Leandro-SI <llmanutencao@bol.com.br>
2019-09-21 14:24:20 -0300

    dependencias do firebase e cadastro de usuario

--- app/src/main/java/com/example/autenticacaofirebase/MainActivity.java
+++ app/src/main/java/com/example/autenticacaofirebase/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.autenticacaofirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentuser = mAuth.getCurrentUser();
+
+        mAuth.createUserWithEmailAndPassword("testeleandro@gmail.com", "842682ll")
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Log.i("AUTH", "usuario cadastrado com sucesso");
+                        }else{
+                            Log.i("AUTH", "Erro ao cadastrar usuario");
+                        }
+                    }
+                });
+
     }
+
+
 }
